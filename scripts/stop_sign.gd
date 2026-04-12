6fcc07c5b06d113fbfe414cc3cf02c80b09b638a
CoolTL <thomasliisborg@gmail.com>
2026-04-12 12:12:53 +0200

    made a stop sign, and added a sign to speed zone

---
+++ scripts/stop_sign.gd
@@ -0,0 +1,48 @@
+extends Area3D
+
+var car_in = false
+var car_stop = false
+var lovbrud = false
+
+@export var stop_sign: MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not car_in:
+		return
+	var car = get_overlapping_bodies().filter(func(b): return b.is_in_group("player_car"))
+	if car[0].linear_velocity.length() <= 0.2:
+		car_stop = true
+
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player_car"):
+		var direction = body.linear_velocity.normalized()
+		var allowed_direction = -stop_sign.global_basis.x.normalized()
+		# tjekker om bilen og skiltet er den samme retning
+		if direction.dot(allowed_direction) > 0.5:
+			car_in = true
+			lovbrud = false
+			car_stop = false
+		
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player_car"):
+		if car_in:
+			if not car_stop:
+				lovbrud = true
+			
+			if lovbrud:
+				print("Du stoppede ikke")
+			else:
+				print("Du stoppede")
+			car_in = false
+		
